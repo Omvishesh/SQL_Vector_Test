4c8977ad912c88a7c83c184f79e96d32fb2fe001
Omvishesh <omvishesh123@gmail.com>
2025-09-18 15:24:38 +0530

    added pandas to check the workflow

--- SQL_Retrieval/api_main.py
+++ SQL_Retrieval/api_main.py
@@ -23,6 +23,7 @@ from   datetime import datetime
 from   handler_sql import batch_sql_queries, handle_forecast #, return_table_list
 from   utils_common import llm_call
 import numpy as np
+import pandas as pd
 #from   async_handler_sql import batch_sql_queries
 
 current_date = datetime.now().strftime('%Y-%m-%d')
