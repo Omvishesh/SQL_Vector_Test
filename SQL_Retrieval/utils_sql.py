468ee29592ff33b7ee510749a1436ff03a579ee3
Omvishesh <omvishesh123@gmail.com>
2025-09-23 15:13:19 +0530

    modified classify query for cement type question classification to IIP(more specific 1.2) [sql]

--- SQL_Retrieval/utils_sql.py
+++ SQL_Retrieval/utils_sql.py
@@ -30,7 +30,7 @@ def classify_query(query):
 
                 ## IIP
                               
-                a. Entities such as IIP, industrial output, industrial production, mining, manufacturing, electricity, motor vehicles,construction material like cement or other industries should be classified as "IIP".
+                a. Entities such as IIP, industrial output, industrial production,material like cement, mining, manufacturing, electricity, motor vehicles or other industries should be classified as "IIP".
                 
                 b. The following files comprise the IIP datasets: [iip_india_yr_catg_view,iip_india_mth_catg_view,iip_india_yr_subcatg_view,iip_india_mth_subcatg_view,iip_in_assam,construct_state_cement_indicators]. 
                 
